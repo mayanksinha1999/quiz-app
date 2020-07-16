99b881ef6a87e8993eb8e2b7233eadc0912216ba
Mayank Sinha <mayank.sinha1999@gmail.com>
2020-07-16 08:29:07 +0530

    first commit

---
+++ src/QuizData.js
@@ -0,0 +1,26 @@
+export const QuizData=[
+    {
+        id:0,
+        question:`What is the Capital of India?`,
+        options:[`New Delhi`,'Mumbai','Chennai',`Kolkata`],
+        answer:`New Delhi`
+    },
+    {
+        id:1,
+        question:`What is the Capital of India?`,
+        options:[`New Delhi`,'Mumbai','Chennai',`Kolkata`],
+        answer:`Mumbai`
+    },
+    {
+        id:2,
+        question:`What is the Capital of India?`,
+        options:[`New Delhi`,'Mumbai','Chennai',`Kolkata`],
+        answer:`Kolkata`
+    },
+    {
+        id:3,
+        question:`What is the Capital of India?`,
+        options:[`New Delhi`,'Mumbai','Chennai',`Kolkata`],
+        answer:`Chennai`
+    },
+]
